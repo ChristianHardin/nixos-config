1ebce63362ca783c47a18250efdb4c998fd593ff
ChristianHardin <christianhardin12345@gmail.com>
2023-11-27 21:02:46 -0500

    fix: fix the broky stuff

--- hosts/common/optional/sddm.nix
+++ hosts/common/optional/sddm.nix
@@ -1,7 +1,7 @@
 {
-  services = {
-    xserver.enable = true;
-    xserver.displayManager.sddm.enable = true;
+  services.xserver = {
+    enable = true;
+    displayManager.sddm.enable = true;
     displayManager.defaultSession = "plasmawayland"; # or whatever you using
   };
 }
