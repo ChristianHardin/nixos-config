e7fa0109e82dd7d9e6960b3a29058a14194db802
ChristianHardin <christianhardin12345@gmail.com>
2023-11-30 23:01:49 -0500

    feat: enable xdg portal

--- hosts/common/desktop/plasma/default.nix
+++ hosts/common/desktop/plasma/default.nix
@@ -7,10 +7,13 @@
     # Enable the Plasma desktop.
     desktopManager.plasma5.enable = true;
   };
-    # Enable Bluetooth support.
-    hardware.bluetooth.enable = true;
-    hardware.bluetooth.powerOnBoot = true;
 
-    # Enable CUPS to print documents.
-    services.printing.enable = true;
+  # Enable Bluetooth support.
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  xdg.portal.enable = true;
 }
