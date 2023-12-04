fd62aa4c09c482562341c717c9ba60d9d72467f6
ChristianHardin <christianhardin12345@gmail.com>
2023-12-03 19:19:27 -0500

    Added nvim plugins

--- home/herman/features/cli/nvim.nix
+++ home/herman/features/cli/nvim.nix
@@ -1,15 +1,88 @@
+{ pkgs, lib, ...}:
 {
   home.sessionVariables.EDITOR = "nvim";
 
-  programs.nvim = {
+  programs.neovim = {
+    defaultEditor = true;
     enable = true;
     viAlias = true;
     vimAlias = true;
-    plugins = [
-      pkgs.vimPlugins.nvim-tree-lua
+    plugins = with pkgs.vimPlugins; [
+      vim-illuminate
+      vim-numbertoggle
       {
-        plugin = pkgs.vimPlugins.vim-startify;
-        config = "let g:startify_change_to_vcs_root = 0";
+        plugin = nvim-web-devicons;
+        type = "lua";
+        config = /* lua */ ''
+          require('nvim-web-devicons').setup()
+        '';
+      }
+      {
+        plugin = nvim-tree-lua;
+        type = "lua";
+        config = /* lua */ ''
+          require('nvim-tree').setup()
+        '';
+      }
+      {
+        plugin = nvim-autopairs;
+        type = "lua";
+        config = /* lua */ ''
+          require('nvim-autopairs').setup()
+        '';
+      }
+      {
+        plugin = lualine-nvim;
+        type = "lua";
+        config = /* lua */ ''
+          require('lualine').setup()
+        '';
+      }
+      {
+        plugin = bufferline-nvim;
+        type = "lua";
+        config = /* lua */ ''
+          require('bufferline').setup{}
+        '';
+      }
+      {
+        plugin = scope-nvim;
+        type = "lua";
+        config = /* lua */ ''
+          require('scope').setup{}
+        '';
+      }
+      {
+        plugin = which-key-nvim;
+        type = "lua";
+        config = /* lua */ ''
+          require('which-key').setup{}
+        '';
+      }
+      {
+        plugin = indent-blankline-nvim;
+        type = "lua";
+        config = /* lua */ ''
+          require('ibl').setup{}
+        '';
+      }
+        {
+        plugin = nvim-colorizer-lua;
+        type = "lua";
+        config = /* lua */ ''
+          require('colorizer').setup{}
+        '';
+      }
+      {
+        plugin = fidget-nvim;
+        type = "lua";
+        config = /* lua */ ''
+          require('fidget').setup{
+            text = {
+              spinner = "dots",
+            },
+          }
+        '';
       }
     ];
   };
