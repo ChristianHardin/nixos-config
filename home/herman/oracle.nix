f1946b070ad0788a5cd8aa6e687712df35b6d408
ChristianHardin <christianhardin12345@gmail.com>
2023-12-04 22:12:55 -0500

    Update monitor configurations

--- home/herman/oracle.nix
+++ home/herman/oracle.nix
@@ -11,13 +11,29 @@
 
   monitors = [
     {
-      name = "DP-1";
+      name = "DP-2";
       width = 2560;
       height = 1440;
       refreshRate = 144;
       x = 0;
+      workspace = "2";
+    }
+    {
+      name = "DP-1";
+      width = 2560;
+      height = 1440;
+      refreshRate = 144;
+      x = 2560;
       workspace = "1";
       primary = true;
+    }
+    {
+      name = "p2211H";
+      width = 1920;
+      height = 1080;
+      refreshRate = 60;
+      x = 4480;
+      workspace = "3";
     }
   ];
 }
