27a2e0fb51d5c01c3f0a89b0e255f3cad0483420
Christian A-H <contact@christianhardin.com>
2025-02-09 23:47:38 -0500

    125 current  2025-02-09 23:47:28  24.11.20250126.4e96537  6.6.72                          *

--- config/desktop/plasma/default.nix
+++ config/desktop/plasma/default.nix
@@ -21,4 +21,10 @@
   # Enable and power on bluetooth
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
+  hardware.bluetooth.settings = {
+    General = {
+      Enable = "Source,Sink,Media,Socket";
+      Experimental = true;
+    };
+  };
 }
