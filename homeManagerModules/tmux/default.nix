ce2cb86cc74891263e4126b5591675fb73f12d8b
Christian A-H <contact@christianhardin.com>
2025-02-16 16:03:32 -0500

    142 current  2025-02-14 00:12:15  24.11.20250126.4e96537  6.6.72                          *

--- homeManagerModules/tmux/default.nix
+++ homeManagerModules/tmux/default.nix
@@ -21,12 +21,6 @@
       plugins = with pkgs; [];
 
       extraConfig = ''
-        # Mouse works as expected
-          	set-option -g mouse on
-          	# easy-to-remember split pane commands
-          	bind | split-window -h -c "#{pane_current_path}"
-          	bind - split-window -v -c "#{pane_current_path}"
-          	bind c new-window -c "#{pane_current_path}"
       '';
     };
   };
