7e27efc677664b6edd0b3939c9110dc53bcde0b3
Christian A-H <contact@christianhardin.com>
2025-02-16 15:38:53 -0500

    142 current  2025-02-14 00:12:15  24.11.20250126.4e96537  6.6.72                          *

--- homeManagerModules/tmux/default.nix
+++ homeManagerModules/tmux/default.nix
@@ -13,10 +13,18 @@
     programs.tmux = {
       enable = true;
       clock24 = true;
+      terminal = "tmux-256color";
       keyMode = "vi";
       mouse = true;
       historyLimit = 100000;
       plugins = with pkgs; [];
+      extraConfig = ''
+        # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
+        set -g default-terminal "xterm-256color"
+        set -ga terminal-overrides ",*256col*:Tc"
+        set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+        set-environment -g COLORTERM "truecolor"
+      '';
     };
   };
 }
