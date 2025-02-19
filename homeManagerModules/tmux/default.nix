d08da54fccd86e040389d12db15d210ad9c8ba17
Christian A-H <contact@christianhardin.com>
2025-02-19 11:57:53 -0500

    153 current  2025-02-19 01:02:09  24.11.20250126.4e96537  6.6.72                          *

--- homeManagerModules/tmux/default.nix
+++ homeManagerModules/tmux/default.nix
@@ -14,14 +14,25 @@
       enable = true;
       shortcut = "a";
       keyMode = "vi";
-      terminal = "xterm-256color";
+      terminal = "xterm-256color:Tc";
       clock24 = true;
       historyLimit = 100000;
-      mouse = true;
 
-      plugins = with pkgs; [];
+      plugins = with pkgs; [tmuxPlugins.better-mouse-mode];
 
       extraConfig = ''
+        # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
+        set -g default-terminal "xterm-256color"
+        set -ga terminal-overrides ",*256col*:Tc"
+        set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+        set-environment -g COLORTERM "truecolor"
+
+        # Mouse works as expected
+        set-option -g mouse on
+        # easy-to-remember split pane commands
+        bind | split-window -h -c "#{pane_current_path}"
+        bind - split-window -v -c "#{pane_current_path}"
+        bind c new-window -c "#{pane_current_path}"
       '';
     };
   };
