ce9a794d31a9f5e53595c74318e2e04a954e1135
Christian A-H <contact@christianhardin.com>
2025-03-01 01:13:03 -0500

    159 current  2025-03-01 01:12:50  24.11.20250126.4e96537  6.6.72                          *

--- hosts/oracle/configuration.nix
+++ hosts/oracle/configuration.nix
@@ -56,6 +56,7 @@
   gnupg.enable = true;
   steam-devices.enable = true;
   umu-launcher.enable = true;
+  podman.enable = true;
 
   networking.hostName = "oracle";
   networking.networkmanager.enable = true;
